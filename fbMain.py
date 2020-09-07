88457def52776c220b54e4bcb997d9b98e935bb2
njm7162 <njm7162@autuni.ac.nz>
2020-09-07 20:50:51 +1200

    inital commit

--- fbMain.py
+++ fbMain.py
@@ -2,7 +2,7 @@ import fbchat
 from fbchat import Client
 from getpass import getpass
 
-username = "NZpatelK"
+username = "****"
 client = fbchat.Client(username, getpass())
 no_of_friends = int(input("Number of friends:"))
 for i in range(no_of_friends):
